28cbb1fbdcc6906899402595390c57f98ac993af
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-22 16:11:28 +0200

    Before refactor, everything works

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,19 +2,5 @@ import { createStore } from "redux";
 import { rootReducer } from "./reducer";
 import { devToolsEnhancer } from "@redux-devtools/extension";
 
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and XXX CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: "all",
-  },
-};
-
 const enhancer = devToolsEnhancer();
-
 export const store = createStore(rootReducer, enhancer);
